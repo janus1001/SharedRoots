d249ffca841a88ec875d17980219cd0e6d940fc0
Murilo Salviato Pileggi <MuzaoDaMassaGames@gmail.com>
2023-02-03 18:37:30 -0300

    Basic player movement
    Created basic isometric movement for the player

---
+++ GGJ_2023/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _turnSpeed = 360;
+
+    private Vector3 _input;
+
+    private void Update()
+    {
+        GatherInput();
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (_input != Vector3.zero)
+        {
+            var relative = (transform.position + _input.ToIso()) - transform.position;
+            var rotation = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+    }
+}
