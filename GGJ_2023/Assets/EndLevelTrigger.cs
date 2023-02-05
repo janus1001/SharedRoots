095a4bbedf9f79e161914a9a86bf0482fedbfaf3
Jan Cenzartowicz <janusz.cimortowicz@gmail.com>
2023-02-05 18:14:18 +0100

    Puzzle 1 done

---
+++ GGJ_2023/Assets/EndLevelTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public Dialogue aaa;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PuzzlePiece"))
+        {
+            DialogueSystem.CreateDialogue(aaa);
+        }
+    }
+}
